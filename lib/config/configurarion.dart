555cc5d382ffed9849f2721544064f74673ca2bb
dxmarquez <113218066+dxmarquez@users.noreply.github.com>
2022-11-30 20:00:00 -0500

    Add files via upload

---
+++ lib/config/configurarion.dart
@@ -0,0 +1,18 @@
+//create a file called configuration.dart on this folder
+// copy the values from firebase
+
+class Configuration {
+  static const apiKey =
+      "AIzaSyAtWX-ESn5Zz85bhiQLUMPh3yk_h5c5tS0"; // tomado de current_key
+  static const authDomain =
+      "reto3-salachat.firebaseapp.com"; // tomado de oauth_client
+  static const databaseURL =
+      "reto3-salachat.firebaseapp.com"; // tomado de firebase_url
+  static const projectId = "reto3-salachat"; // tomado de project_id
+  static const storageBucket =
+      "reto3-salachat.appspot.com"; // tomado de storage_bucket
+  static const messagingSenderId = "973838687589"; //no lo vamos a usar
+  static const appId =
+      "1:973838687589:web:4e7b536c3cceb4e974558c"; // tomado de mobilesdk_app_id
+  static const measurementId = ""; //no lo vamos a usar
+}
